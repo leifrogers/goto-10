fe35307bdfa02e05ca5e9654be9d93a70e4da1dc
Leif Rogers <leif@leifrogers.com>
2018-11-16 17:28:25 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# goto10
+
+small recreation of the storied <a href="https://10print.org/">10 Print</a>
+
+while the javascript is limited to one giant line, the html demanded formatting into a div.
---
+++ js/goto10.js
@@ -0,0 +1,6 @@
+/**
+ * Absurd recreation of the storied goto10 function
+ *
+ */
+
+setInterval(function () {Math.random() > 0.49 ? document.getElementById("goto10").insertAdjacentHTML("beforeend", '⧸') : document.getElementById("goto10").insertAdjacentHTML("beforeend", '⧹');}, 1);
