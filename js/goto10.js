9506ff18cd4ac47576879249917ddbc77a527283
Leif Rogers <leif@leifrogers.com>
2018-11-17 00:25:17 -0600

    slash changes

--- js/goto10.js
+++ js/goto10.js
@@ -1,8 +1,9 @@
 /**
  * Absurd recreation of the storied goto10 function
  *
- * uses U+29F8 (big solidus) and U+29F9 (big reverse solidus)
- * for the slashes since it better fills the line
+ * used to use U+29F8 '⧸' (big solidus) and U+29F9 '⧹' (big reverse solidus)
+ * for the slashes since it better fills the line but switched back to normal
+ *  /\ as test for edge, etc.
  */
 
-setInterval(function () {Math.random() > 0.49 ? document.getElementById("goto10").insertAdjacentHTML("beforeend", '⧸') : document.getElementById("goto10").insertAdjacentHTML("beforeend", '⧹');}, 1);
+setInterval(function () {Math.random() > 0.49 ? document.getElementById("goto10").insertAdjacentHTML("beforeend", '/') : document.getElementById("goto10").insertAdjacentHTML("beforeend", '\\');}, 1);
