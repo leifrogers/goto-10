56b46db36b96131213c5c6350cf908dde4a13741
Leif Rogers <leif@leifrogers.com>
2018-11-17 00:30:40 -0600

    switch back to soliduses

--- js/goto10.js
+++ js/goto10.js
@@ -1,9 +1,15 @@
 /**
  * Absurd recreation of the storied goto10 function
  *
- * used to use U+29F8 '⧸' (big solidus) and U+29F9 '⧹' (big reverse solidus)
- * for the slashes since it better fills the line but switched back to normal
- *  /\ as test for edge, etc.
+ * uses U+29F8 (big solidus) and U+29F9 (big reverse solidus)
+ * for the slashes since it better fills the line - this appears
+ * to screw up microsoft edge and chrome (at least on a surface go)
+ * but firefox holds true.
+ * 
+ * i tried normal / and \ but the word wrap was breaking along with
+ * unwanted line spaces, etc.  so the above currently seems the lesser
+ * of evils here.
+ * 
  */
 
-setInterval(function () {Math.random() > 0.49 ? document.getElementById("goto10").insertAdjacentHTML("beforeend", '/') : document.getElementById("goto10").insertAdjacentHTML("beforeend", '\\');}, 1);
+setInterval(function () {Math.random() > 0.49 ? document.getElementById("goto10").insertAdjacentHTML("beforeend", '⧸') : document.getElementById("goto10").insertAdjacentHTML("beforeend", '⧹');}, 1);
