bde711aba5b01ae055526e4d68be44eb35fd3450
Leif Rogers <leif@leifrogers.com>
2018-11-16 23:43:10 -0600

    updated comment to include unicode for soliduses

--- js/goto10.js
+++ js/goto10.js
@@ -1,6 +1,8 @@
 /**
  * Absurd recreation of the storied goto10 function
  *
+ * uses U+29F8 (big solidus) and U+29F9 (big reverse solidus)
+ * for the slashes since it better fills the line
  */
 
 setInterval(function () {Math.random() > 0.49 ? document.getElementById("goto10").insertAdjacentHTML("beforeend", '⧸') : document.getElementById("goto10").insertAdjacentHTML("beforeend", '⧹');}, 1);
